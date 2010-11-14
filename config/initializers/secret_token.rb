d7622290ae4637f9eb1e6691e2263f6e0752cc7b
lothmak <lothmak@gmail.com>
2010-11-14 19:38:44 -0400

    Commit Inicial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mlog::Application.config.secret_token = '7458970a0c540c61d4f3d9acbe45438da51e43a706824947df82be50c185f0af733d3aa0729724463c220ca240fb8bdf2798a0f5a478eab3133aca0b6f7677ee'
