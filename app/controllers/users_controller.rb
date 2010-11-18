d3f1363753cbd2bdb416808f529864edd9fc5541
lothmak <lothmak@gmail.com>
2010-11-18 15:23:00 -0400

    Done with sign in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,14 +12,14 @@ class UsersController < ApplicationController
   def create
     @user = User.new(params[:user])
     if @user.save
-      flash[:success] = "Welcome to Mlog!"
+      sign_in @user
+      flash[:success] = "Welcome to the Sample App!"
       redirect_to @user
     else
       @title = "Sign up"
       render 'new'
     end
   end
-end
 
 
 
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,4 +9,8 @@ module ApplicationHelper
       "#{base_title} | #{@title}"
     end
   end
+  def logo
+    image_tag("logo.png", :alt => "Mlog xD", :class => "round")
+  end
 end
+
